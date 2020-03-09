0efbed10c4220636012fcc462c8767d9e81261e2
profjoaomenk <59892681+profjoaomenk@users.noreply.github.com>
2020-03-09 15:39:17 -0300

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,1 +0,0 @@
-set MAVEN_OPTS=-Djava.net.preferIPv4Stack=true -Djava.net.preferIPv6Addresses=false bash /home/site/repository/apache-maven-3.3.9/bin/mvn clean install -Ddir=/home/site/wwwroot/webapps
