2bdea93dbf743b59640b2bf9a623733262991f85
profjoaomenk <59892681+profjoaomenk@users.noreply.github.com>
2020-03-09 11:00:20 -0300

    Versão Linux

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-set MAVEN_OPTS=-Djava.net.preferIPv4Stack=true -Djava.net.preferIPv6Addresses=false \apache-maven-3.3.9\bin\mvn clean install -Ddir=D:\home\site\wwwroot\webapps
+set MAVEN_OPTS=-Djava.net.preferIPv4Stack=true -Djava.net.preferIPv6Addresses=false /apache-maven-3.3.9/bin/mvn clean install -Ddir=/home/site/wwwroot/webapps
