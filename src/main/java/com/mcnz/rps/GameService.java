ec870471e7df7ec25df3c47e03866ca4dee78c28
Roberto <roberto.menesantos@gmail.com>
2024-10-15 08:36:48 -0300

    alterando a logica da selecao de jogadas pelo computador e como a vitoria e validada

--- src/main/java/com/mcnz/rps/GameService.java
+++ src/main/java/com/mcnz/rps/GameService.java
@@ -1,21 +1,33 @@
 package com.mcnz.rps;
 
+import java.util.HashMap;
+
 public class GameService {
-	
+	HashMap<String, Integer> jogadas = new HashMap<String, Integer>() {{
+		put("rock", 1);
+		put("paper", 2);
+		put("scissors", 3);
+	}};
+
+	private Integer modulo (int a, int b){
+		return ((a % b) + b) % b;
+	}
 
 	public GameSummary playRoshambo(String clientGesture) {
-		
-		String result = "error";
-		if (clientGesture.equals("scissors")) {
+
+		String result;
+		int jogadaComputador = (int) Math.floor(Math.random() * jogadas.size());
+		String valorJogadaComputador = jogadas.keySet().toArray(new String[0])[jogadaComputador];
+
+		if((this.modulo(jogadas.get(clientGesture) - jogadas.get(valorJogadaComputador), jogadas.size()) % 2) == 1) {
+			result = "win";
+		}else if((jogadas.get(clientGesture) - jogadas.get(valorJogadaComputador)) == 0){
+			result = "tie";
+		}else{
 			result = "lose";
 		}
-		if (clientGesture.equals("paper")) {
-			result = "win";
-		}
-		if (clientGesture.equals("rock")) {
-			result = "tie";
-		}
-		GameSummary gameSummary = new GameSummary(clientGesture, "rock", result);
+
+		GameSummary gameSummary = new GameSummary(clientGesture, valorJogadaComputador, result);
 		return gameSummary;
 	}
 
